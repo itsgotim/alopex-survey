ee423a0421c3311c27ebcb8aa20d1354b29133db
itsgotim <timschorr@gmail.com>
2018-11-08 20:22:04 -0900

    Added tracking of sections and their percentage complete, slick slider, transitioning background image, custom post type and taxonomy

--- assets/js/survey.js
+++ assets/js/survey.js
@@ -1,9 +1,49 @@
 jQuery(document).ready(function(){
+    //Initiate slick slider
     jQuery('.slider').slick({
-        draggable: false
+        draggable: false,
+        infinit: false
     });
-    jQuery("input[type='radio']").change(function(){
-        // Do something interesting here
+    
+    //Get page count from hidden form element
+    var pages = jQuery("input[name=alos_pagecount").val();
+    var total_points = jQuery("input[name=alos_totalpoints").val();
+    console.log(pages);
+    
+    //Do things when any radio button is clicked
+    jQuery("input[type='radio']").change(function() {
+        jQuery('.slider').slick('slickNext');
+
+        //Loop through radio button sections
+        var i = 1;
+        for(i = 1; i <= pages; i++) {
+            var section_points = 0;
+            jQuery('.sect'+i+':checked').each(function() {
+                section_points += parseInt(jQuery(this).val());
+            });
+            jQuery('#alosurvey_section'+i+'_points').html(section_points);
+            console.log('section'+i);
+        }
+
+        //Loop through all radio buttons, regardless of section
+        var curr_points = 0;
+        var curr_questions = 0;
+        jQuery(".alosurvey_radio:checked").each(function() {
+            curr_points = ( curr_points + parseInt(jQuery(this).val()) );
+            curr_questions++;
+        });
+        jQuery('#alosurvey_totalq').html(curr_questions);
+        jQuery('#alosurvey_totalp').html(curr_points);
+
+        //Update background image
+        var bgpos = 0;
+        bgpos = Math.round((curr_points / total_points) * 100);
+        jQuery('#alosurvey_container').css('background-position-y', (100-bgpos)+'%');
+        //console.log('points:'+curr_points + ' total_points:'+total_points+' bgpos:'+bgpos); 
+        
+    })
+
+    jQuery(".alosurvey_button").click(function() {
         jQuery('.slider').slick('slickNext');
     });
 });
--- inc/shortcode.php
+++ inc/shortcode.php
@@ -4,66 +4,84 @@
  *  */
 
 function alopex_survey_shortcode() {
-
-    //Register our URL variable
-    /*function add_get_val() { 
-        global $wp; 
-        $wp->add_query_var('qpage'); 
-    }
-    add_action('init','add_get_val');*/
+    $theoutput = null;
+    $alo_query_total = null;
+    $alo_query = null;
 
     //Get the terms/pages (qgroup) from our custom taxonomy
     $terms = get_terms( array(
         'taxonomy' => 'qgroup',
-        'hide_empty' => false,
+        'hide_empty' => true,
     ) );
+
+    //Term object output
     echo '<!--';
     print_r($terms);
     echo '-->';
-    foreach($terms as $term) {
-        echo '<a href="' . add_query_arg( 'qpage', $term->slug ) . '">' . $term->slug . '</a> | ';
-    }
-
-    echo '<br>Current: '.$_GET['qpage'];
-
-    $args = array(
+    
+    //Get total questions
+    $args_total = array(
         'post_type' => 'alopex_survey',
         'post_status' => 'publish',
-        'posts_per_page' => '-1',
-        'tax_query' => array(
-            array(
-                'taxonomy' => 'qgroup',
-                'field'    => 'slug',
-                'terms'    => $_GET['qpage'],
-            ),
-        ),
+        'posts_per_page' => '-1', //Get ALL posts
     );
+    $alos_query_total = new WP_query($args_total);
 
-    $alo_query = null;
-    $theoutput = null;
-    $alo_query = new WP_query($args);
-    echo '<br># of Questions: ' . $alo_query->post_count;
-    if($alo_query->have_posts()):
-        $theoutput = '<form action="" name="alopex_survey" id="alopex_survey">'."\n"
-                    .'<div id="alopex_survey_container" class="slider">'."\n";
-        $i = 0;
-        while($alo_query->have_posts()) : $alo_query->the_post();
-            $i++;
-            $custom = get_post_custom( get_the_ID() );
-            $theoutput .= '<div><h4>'.get_the_content().'</h4>'."\n"
-                    .'<fieldset id="q'.$i.'">'."\n"
-                    .'<input type="radio" name="q'.$i.'" id="always'.$i.'" value="' . alopex_survey_get_meta( 'alopex_survey_answer01_points' ) . '"><label class="survey-label" for="always'.$i.'">' . alopex_survey_get_meta( 'alopex_survey_answer01_name' ) . '</label>'."\n"
-                    .'<input type="radio" name="q'.$i.'" id="occa'.$i.'" value="' . alopex_survey_get_meta( 'alopex_survey_answer02_points' ) . '"><label class="survey-label" for="occa'.$i.'">' . alopex_survey_get_meta( 'alopex_survey_answer02_name' ) . '</label>'."\n"
-                    .'<input type="radio" name="q'.$i.'" id="never'.$i.'" value="' . alopex_survey_get_meta( 'alopex_survey_answer03_points' ) . '"><label class="survey-label" for="never'.$i.'">' . alopex_survey_get_meta( 'alopex_survey_answer03_name' ) . '</label>'."\n"
-                    .'</fieldset></div>'."\n";
-        endwhile;
+    if ($alos_query_total->have_posts()) {
+        $theoutput = '<span id="alosurvey_totalq">0</span> of '.$alos_query_total->post_count.' total questions.<br>'
+                    .'<span id="alosurvey_totalp">0</span> of '.($alos_query_total->post_count * 5).' total points.<br><br>'
+                    .'<form action="" name="alourvey" id="alosurvey_form">'."\n"
+                    .'<input type="hidden" name="alos_pagecount" value="'.wp_count_terms( 'qgroup' ).'">'."\n" //Pass javascript this section's number of sections
+                    .'<input type="hidden" name="alos_totalpoints" value="'.($alos_query_total->post_count * 5).'">'."\n" //Pass jscript total points possible
+                    .'<div id="alosurvey_container" class="slider">'."\n";
+        //For each term in our taxonomy (page of questions) create a query and loop through posts, outputting progress at end of each 
+        $p = 0;
+        foreach($terms as $term) {
+            $p++;
+            $args_term = array(
+                'post_type' => 'alopex_survey',
+                'post_status' => 'publish',
+                'posts_per_page' => '-1', //Get ALL posts
+                'tax_query' => array( //...in current term
+                    array(
+                        'taxonomy' => 'qgroup',
+                        'field'    => 'slug',
+                        'terms'    => $term,
+                    ),
+                ),
+            );
+
+            
+            $alos_query = new WP_query($args_term);
+            
+            //Loop through current page of questions
+            if($alos_query->have_posts()) {
+                $i = 0;
+                while($alos_query->have_posts()) : $alos_query->the_post();
+                    $i++;
+                    //$custom = get_post_custom( get_the_ID() );
+                    $pi = $p.'-'.$i;
+                    $theoutput .= '<div class="alosurvey_slide"><h4>'.get_the_content().'</h4>'."\n"
+                            .'<fieldset id="q'.$p.'-'.$i.'">'."\n"
+                            .'<input type="radio" name="q'.$pi.'" id="alosurvey_always'.$pi.'" class="alosurvey_radio sect'.$p.'" value="' . alopex_survey_get_meta( 'alopex_survey_answer01_points' ) . '"><label class="alosurvey_label" for="alosurvey_always'.$pi.'">' . alopex_survey_get_meta( 'alopex_survey_answer01_name' ) . '</label>'."\n"
+                            .'<input type="radio" name="q'.$pi.'" id="alosurvey_occa'.$pi.'" class="alosurvey_radio sect'.$p.'" value="' . alopex_survey_get_meta( 'alopex_survey_answer02_points' ) . '"><label class="alosurvey_label" for="alosurvey_occa'.$pi.'">' . alopex_survey_get_meta( 'alopex_survey_answer02_name' ) . '</label>'."\n"
+                            .'<input type="radio" name="q'.$pi.'" id="alosurvey_never'.$pi.'" class="alosurvey_radio sect'.$p.'" value="' . alopex_survey_get_meta( 'alopex_survey_answer03_points' ) . '"><label class="alosurvey_label" for="alosurvey_never'.$pi.'">' . alopex_survey_get_meta( 'alopex_survey_answer03_name' ) . '</label>'."\n"
+                            .'</fieldset></div>'."\n";
+                    if ($i == $alos_query->post_count) { //need better logic for tracking section ends............
+                        $theoutput .= '<div class="alosurvey_slide">Hey, you reached the end of this section, go you!<br>'."\n"
+                                .'You scored <span id="alosurvey_section'.$p.'_points">0</span> out of <span id="alosurvey_section1_total">'.($alos_query->post_count * 5).'</span> possible points on this section.<br>'."\n"
+                                .'<button type="button" class="alosurvey_button">Next Section</button></div>'."\n";
+                    }
+                endwhile;
+                wp_reset_postdata();
+            } else {
+            _e( 'Sorry, no posts matched your criteria.' );
+            }
+        }
         $theoutput .= '</div>'."\n"
-                    .'</form>'."\n";
+            .'</form>'."\n";
         return $theoutput;
-        wp_reset_postdata();
-    else :
-    _e( 'Sorry, no posts matched your criteria.' );
-    endif;
+    }
 
 }
 add_shortcode('alo_survey', 'alopex_survey_shortcode');
